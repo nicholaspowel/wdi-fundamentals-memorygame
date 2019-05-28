c55a0c5e78a4c25643ae6d26aea5627636caf94f
Nick <nicholaspowel@users.noreply.github.com>
2019-05-28 17:10:20 -0400

    flipFunction

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,22 +1,14 @@
 var cards = ["queen","queen","king","king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-
-cardsInPlay.push(cardOne);
-
-console.log("User flipped " + cardsInPlay[0]);
-
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardsInPlay[1]);
-
-if (cardsInPlay.length===2) {
-	if (cardsInPlay[0]===cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again.");
+function flipCard(cardId) {
+	
+	if (cardsInPlay.length===2) {
+		if (cardsInPlay[0]===cardsInPlay[1]) {
+			alert("You found a match!");
+		} else {
+			alert("Sorry, try again.");
+		}
 	}
-}
+};
+
