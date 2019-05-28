72a1f6664937df98a34e3ea52c4d6ecd8caebeba
Nick <nicholaspowel@users.noreply.github.com>
2019-05-28 17:23:07 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,14 +1,20 @@
 var cards = ["queen","queen","king","king"];
 var cardsInPlay = [];
-
-function flipCard(cardId) {
-	
-	if (cardsInPlay.length===2) {
-		if (cardsInPlay[0]===cardsInPlay[1]) {
+function checkForMatch() {
+	if (cardsInPlay[0]===cardsInPlay[1]) {
 			alert("You found a match!");
 		} else {
 			alert("Sorry, try again.");
 		}
+}
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length===2) {
+		checkForMatch();
 	}
 };
+
+flipCard(0);
+flipCard(2);
 
